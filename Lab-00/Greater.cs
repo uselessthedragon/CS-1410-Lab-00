16de42d0ec6bdd8f9ad3d85e10b86e616ebd487b
Anthony Hardman <anthony.hardman@ad.snow.edu>
2021-01-12 08:04:52 -0700

    Add project files.

---
+++ Lab-00/Greater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_00
+{
+    public static class Greater
+    {
+        public static int Greatest(int num1, int num2)
+        {
+            int greatest = 0;
+
+            if (num1 >= num2)
+            {
+                greatest = num1;
+            }
+            else if (num2 > num1)
+            {
+                greatest = num2;
+            }
+
+            return greatest;
+        }
+    }
+}
---
+++ Lab-00/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Lab_00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.Write("Please enter integer: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter another integer: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The greatest number is ");
+            Console.Write(Greater.Greatest(num1, num2));
+        }
+    }
+}
